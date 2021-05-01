372643987edb2b1b486977ff3f5440ff50ab3da7
J33sus <twwtingprueb@gmail.com>
2021-05-01 19:46:20 -0400

    GTA SA Menu (First version)

---
+++ README.md
@@ -0,0 +1,4 @@
+# GTA SA Menu
+A simple GTA San Andreas Menu with HTML, CSS and JS (Audio).
+
+> Resources extracted from the game.
---
+++ js/gtasamenu.js
@@ -0,0 +1,6 @@
+let menuAudio = new Audio('./sounds/button.wav');
+let menuOptions = document.getElementsByClassName('menu-option');
+
+Array.from(menuOptions).forEach(option => {
+	option.addEventListener('mouseenter', menuAudio.play());
+});
